1865604000f1f7abfc0ee8f17c86620a288d9434
Xavier MARIOT <xavier.mariot@gadz.org>
2015-08-06 17:47:44 +0200

    Update kinematics.py ---> tout fonctionne git status git status git status git status git status

--- Environnement_programmation/Codes Xavier Mariot/kinematics.py
+++ Environnement_programmation/Codes Xavier Mariot/kinematics.py
@@ -7,20 +7,19 @@ Created on Mon Aug  3 12:42:24 2015
 """
 
 import numpy as np
-from scipy.stats import threshold
 import math
 
 
 """     --- Description jacobian ---
 
-jacobian calcul la jacobienn de la fonction vectorielle f au point q.
+jacobian calcul la jacobienne de la fonction vectorielle f au point q.
 nb_q est le nombre de paramètres que prend f c.à.d. la longueur de la liste q
 On peut régler le pas utilisé pour calculer la dérivée grâce au paramètre h.
 La dérivée est calculée par la méthode des acroissements finis.
 jacobian retourne une matrice de type numpy.matrix
         --- Fin description ---
 """
-def jacobian(f,nb_q,q,h=0.01):
+def jacobian(f,nb_q,q,h=0.001):
     """     === Description des arguments ===
     f       --->    Fonction de cinématique directe qui retourne un numpy.array
     nb_q    --->    'int' qui indique le nombre de paramétres de f contenu dans q
@@ -46,10 +45,72 @@ def jacobian(f,nb_q,q,h=0.01):
         j.append(1/h*(f(q+dh)-f(q))) # On calcule df/dqi par la formule des accroissements finis
         dh[i] = 0 # On remet la ième composante de dh à zéro pour la suite
     
-    j = np.matrix(j) # On transforme j en matrice, pour l'instant on a en fait la transposée de la jacobienne de f en q
-    # j = threshold(j.T,1e-13) # On obtient la jacobienne en tronquant les zéros numériques
+    j = np.matrix(j) # On transforme j en matrice, pour l'instant on a en stocké la transposée de la jacobienne de f en q
     
     return j.T
+
+
+"""     === Description ik_num_base ===
+
+Cette fonction est l'algorithme de cinématique inverse pour des petits déplacements
+autour d'une position initiale. La position initiale est donnée par le paramètre qi.
+Le petit déplacement par rapport à la position xi=f(qi) est noté dxf.
+La position finale visée vaut xf = xi + dxf
+
+
+"""    
+def ik_num_base(f, nb_q, qi, dxf, it=1, imax=10):
+    """     === Descriptions des arguments ===
+    
+    f           --->    'arg: nd.array 1D; return: nd.array 1D' fonction vectorielle représentative du modèle de cinématique directe
+    nb_q        --->    'int' qui représente le nombre de paramètres d'entrée que prend la fonction f, 
+                    c.à.d. la taille de la liste q.
+    qi          --->    'numpy.array 1D' position initiale du système
+    dxf         --->    'numpy.array 1D' déplacement infinitésimal du système par rapport à sa position initale
+    it          -->     'float' tolérence admise entre le résultat retourné et la position finale souhaitée (norme de la différence)
+    imax        --->    'int' nombre maximal d'itérations à partir duquel l'algorithme de Jacobi s'arrête.
+    
+            === Fin de la description ===
+    """    
+    
+    """     --- Initialisation ---      """
+    q = qi # on initialise la valeur des paramètres q
+    x = f(q) # on enregistre la position initiale du système
+    
+    xf = x + dxf # on calcule la position finale à atteindre
+    dx = dxf # on initialise la variation à effectuer pour se rapprocher de la position finale
+    
+    
+    """     --- Recurrence ---      """
+    for i in range(imax):
+        # Calcul du nouveau dx au format 'numpy.array 1D'
+        dx = xf-x
+        
+        # vérification de la condition d'arrêt
+        if np.linalg.norm(dx) <= it :
+           break
+        else:
+            # mise en forme de dx à l'itération considérée
+            dx = np.matrix(dx)
+            dx = dx.T # dx est maintenant un vecteur colonne qui va pouvoir être mutiplié par une matrice
+            
+            # Calcul numérique de la jacobienne j au point considéré
+            j = jacobian(f,nb_q,q,1e-6)
+            
+            
+            # incrémentation de q au point considéré en gardant le type 'numpy.array 1D'
+            # NB : j.I retourne la pseudo inverse de j
+            q = q + (j.I*dx).T.getA()[0]
+            
+            
+            # mise à jour de x
+            x = f(q)
+        
+        # si la condition d'arrêt n'est pas vérifiée, on itère le calcul jusqu'à imax fois
+    
+    # A la sortie de la méthode de jacobi, q vérifie ||xfinal - f(q)|| <= it
+    return q
+
 
 """     --- Description ik_num ---
 
@@ -59,7 +120,7 @@ d'obtenir la position xfinal en sortie.
 
         --- Fin de la description ---
 """
-def ik_num(f, nb_q, map_qinit, xfinal, it=1, imax=50):
+def ik_num(f, nb_q, map_qinit, xfinal, it=1, imax=10):
     """     === Descriptions des arguments ===
     
     f           --->    'arg: nd.array 1D; return: nd.array 1D' fonction vectorielle représentative du modèle de cinématique direct
@@ -91,122 +152,41 @@ def ik_num(f, nb_q, map_qinit, xfinal, it=1, imax=50):
             imin = i
     
     # On fixe la position initiale à partir de la plus petite distance trouvée.
-    qinit = map_qinit[imin]
-    xinit = map_xinit[imin]
+    q = map_qinit[imin]
+    x = map_xinit[imin]
     
-    print "ik: qinit = ", qinit
-    print "ik: xinit = ", xinit
     
     """ Décoposition du trajet entre xinit et xfinal en plusieurs petits déplacements """
-    """
-    delta_x = xfinal-xinit # calcul du vecteur déplacement total.
-    nb_div = np.floor(np.linalg.norm(delta_x)/(5*it)) + 1 # l'unité de découpage est arbitrairement 5*it
+    
+    delta_x = xfinal-x # calcul du vecteur déplacement total.
+    
+    # l'unité de découpage est arbitrairement 5*it
+    nb_div = int(np.floor(np.linalg.norm(delta_x)/(10*it)) + 1)
+    
+    
     if nb_div > 1:
         dx = (1/float(nb_div))*delta_x
     
     xf=[] # xf sera la liste des points intermédiaires à atteindre.
-    for i in range(int(nb_div + 1)): # on crée la liste des points intermédiaires
-        xf.append(xinit + i*dx)
+    for i in range(nb_div + 1): # on crée la liste des points intermédiaires
+        xf.append(x + i*dx)
     
     xf[-1] = xfinal # On s'assure que la dernière valeur ne sera pas altérée par des erreurs de calcul
-    """    
+    xf.pop(0) # On retire la position intiale
     
-    """ Application de la méthode de jacobi pour chaque trajet intermédiaire """
-    """
+    
+    """ Application de la méthode de jacobi pour chaque trajet intermédiaire avec ik_num_base """
+    
     for xi in xf:
-        for i in range(imax):
-            # calcul de dx à l'itération considérée
-            dx = xi-xinit
-            dx = np.matrix(dx)
-            dx = dx.T # dx est maintenant un vecteur colonne qui va pouvoir être mutiplié par une matrice
-            
-            # Calcul numérique de la jacobienne j au point considéré
-            j = jacobian(f,nb_q,qinit,it/10000.)
-            
-            
-            # incrémentation de qinit au point considéré
-            # NB : j.I retourne la pseudo inverse de j
-            qinit = qinit + (j.I*dx).T.getA()[0]
-            
-            # mise à jour de xinit
-            xinit = f(qinit)
-            
-            # vérification de la condition d'arrêt
-            if np.linalg.norm(xi-xinit) <= it :
-                break
-            # si la condition d'arrêt n'est pas vérifiée, on itère le calcul jusqu'à imax fois 
-    """
-    xi = xfinal
-    for i in range(imax):
-        # calcul de dx à l'itération considérée
-        dx = xi-xinit
-        dx = np.matrix(dx)
-        dx = dx.T # dx est maintenant un vecteur colonne qui va pouvoir être mutiplié par une matrice
+        # le petit déplacement vaut la différence entre la position finale intermédiare visée et la position théorique actuelle
+        dx = xi-f(q)
         
-        # Calcul numérique de la jacobienne j au point considéré
-        j = jacobian(f,nb_q,qinit,1e-6)
-        
-        
-        # incrémentation de qinit au point considéré
-        # NB : j.I retourne la pseudo inverse de j
-        qinit = qinit + (j.I*dx).T.getA()[0]
-        print i, " : ", qinit
-        # mise à jour de xinit
-        xinit = f(qinit)
-        print i, " : ", xinit
-        
-        # vérification de la condition d'arrêt
-        if np.linalg.norm(xi-xinit) <= it :
-            break
-        # si la condition d'arrêt n'est pas vérifiée, on itère le calcul jusqu'à imax fois
+        # Calcul de la nouvelle valeur de q par la méthode de Jacobi pour les petits déplacements
+        q = ik_num_base(f, nb_q, q, dx, it, imax)
     
-    print i
-    # A la sortie de la méthode de jacobi, qinit vérifie ||xfinal - f(qinit)|| <= it
-    return qinit
-    
-def ik_num_base(f, nb_q, qi, dxf, it=1, imax=50):
-    q = qi # on initialise la valeur des paramètres q
-    x = dk_l_hand(qinit) # on enregistre la position initiale du système
-    
-    print "ik: qi = ", q
-    print "ik: xi = ", x
-    
-    xf = x + dxf # on calcule la position finale à atteindre
-    dx = dxf # on initialise la variation à effectuer pour se rapprocher de la position finale
-    
-    for i in range(imax):
-        # mise en forme de dx à l'itération considérée
-        dx = np.matrix(dx)
-        dx = dx.T # dx est maintenant un vecteur colonne qui va pouvoir être mutiplié par une matrice
-        
-        print "dx = ",dx
-        # Calcul numérique de la jacobienne j au point considéré
-        j = jacobian(f,nb_q,q,1e-6)
-        
-        print "j = ",j
-        
-        # incrémentation de q au point considéré
-        # NB : j.I retourne la pseudo inverse de j
-        q = q + (j.I*dx).T.getA()[0]
-        
-        print i, " : ", q
-        
-        # mise à jour de x
-        x = f(q)
-        print i, " : ", x
-        
-        # Calcul du nouveau dx au format 'numpy.array 1D'
-        dx = xf-x
-        
-        # vérification de la condition d'arrêt
-        if np.linalg.norm(dx) <= it :
-            break
-        # si la condition d'arrêt n'est pas vérifiée, on itère le calcul jusqu'à imax fois
-    
-    print i
-    # A la sortie de la méthode de jacobi, qinit vérifie ||xfinal - f(qinit)|| <= it
+    # A la sortie de la méthode de jacobi, qi vérifie ||xfinal - f(q)|| <= it
     return q
-    
+
 
 """     --- Définition de dk_l_hand    ---
 
@@ -246,14 +226,19 @@ if __name__=='__main__':
     print "xinit th = ",xi
     print "xfinal th = ",xfinal
     
-    print jacobian(dk_l_hand,4,qinit,1e-6)
-    
+    print "\n\n"
     
     qf = ik_num_base(dk_l_hand,4,qinit,dx)
+    qfbis = ik_num(dk_l_hand,4,np.array([qinit,[0,0,0,0]]),xfinal)
+    
     print "xfinal calc = ",dk_l_hand(qf)
+    print "xfinal bis calc = ",dk_l_hand(qfbis)
+    print "\n\n"
     
     qf = map(math.degrees,qf)
+    qfbis = map(math.degrees,qfbis)
     
     print "qf en degree = ",qf
+    print "qfbis en degree = ",qfbis
      
     
